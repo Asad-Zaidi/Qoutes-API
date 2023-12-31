13f8a704ed73161daa5b2112e10933fbf5c026dc
Asad-Zaidi <syedasad1410@gmail.com>
2023-12-31 21:42:47 +0500

    some setting change

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/api/users");
 var qoutesRouter = require('./routes/api/qoutes');
 var config = require("config");
-const punycode = require("punycode");
+
 var app = express();
 require("dotenv").config();
 const cors = require('cors');
