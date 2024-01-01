ccc03dcf8c528dbba1c83cb8ba254efafe4fef37
Asad-Zaidi <syedasad1410@gmail.com>
2024-01-01 22:34:43 +0500

    code set

--- index.js
+++ index.js
@@ -149,7 +149,6 @@ function closeEditForm() {
 
 document.body.innerHTML += '<div id="overlay" class="overlay"></div>';
 
-
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
